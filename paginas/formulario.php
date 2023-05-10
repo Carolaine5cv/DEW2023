7eca2d7a167585d7a89f7f98f56756ffaaf40652
Carolaine5cv <carolainevictoria8@gmail.com>
2023-05-10 10:10:44 -0300

    jjj

--- paginas/formulario.php
+++ paginas/formulario.php
@@ -1,2 +1,10 @@
-ph<?php
-$nome=$_POST'nome_usuario']
+<?php
+$nome=$_POST['nome_usuario'];
+$senha=$_POST['senha'];
+echo "Seu nome é ".$nome."e sua senha é ".$senha;
+if ($senha=="")
+{
+    echo "<br> Sua senha está vazia";
+}
+
+?>
